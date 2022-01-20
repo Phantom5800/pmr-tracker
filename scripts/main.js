509965d50f1c59252645a84324a84b8c4fbcf834
Phantom <phantom@phantom-games.com>
2022-01-19 23:51:02 -0800

    Basic tracker functionality

---
+++ scripts/main.js
@@ -0,0 +1,113 @@
+const maxKeyCounts = {
+    1: 4,
+    2: 4,
+    3: 3,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 5
+};
+
+var currentKeyCounts = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+}
+
+var chapterState = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+};
+
+var chapterRequirements = {
+    1: 4,
+    2: 8,
+    3: 4,
+    4: 1,
+    5: 1,
+    6: 7,
+    7: 5,
+    8: 5
+}
+
+
+$(document).ready(function(){
+    // disable some basic functionality
+    $('img').on('dragstart', function(){return false;});
+    $('html').contextmenu(function(){return false;});
+    $('img').contextmenu(function(){return false;});
+
+    $('.star-spirit').height(60);
+    $('.partner').height(60);
+    $('.key-item, .optional-item').height(40);
+
+    $('.partner, .optional-item').click(function() {
+        if ($(this).hasClass("unselected")) {
+            $(this).removeClass("unselected");
+        } else {
+            $(this).addClass("unselected");
+        }
+    });
+
+    // add all the tracker hooks for clicking on images
+    for (var i = 1; i <= 8; ++i) {
+        // required chapter items
+        $(`*[data-chapter="${i}"]`).click(function(){
+            var c = parseInt($(this).attr("data-chapter"));
+            if ($(this).hasClass("unselected")) {
+                $(this).removeClass("unselected");
+                --chapterState[c];
+            } else {
+                $(this).addClass("unselected");
+                ++chapterState[c];
+            }
+        });
+
+        // chapter keys
+        $(`*[data-chapter-key="${i}"]`).click(function(){
+            var c = parseInt($(this).attr("data-chapter-key"));
+            $(this).removeClass("unselected");
+            if (currentKeyCounts[c] < maxKeyCounts[c]) {
+                ++currentKeyCounts[c];
+                ++chapterState[c];
+                $(`#chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
+            }
+        });
+
+        $(`*[data-chapter-key="${i}"]`).contextmenu(function(){
+            var c = parseInt($(this).attr("data-chapter-key"));
+            if (currentKeyCounts[c] > 0) {
+                --currentKeyCounts[c];
+                --chapterState[c];
+                $(`#chapter-${c}-key-count`).text(`${currentKeyCounts[c]}/${maxKeyCounts[c]}`);
+            }
+
+            if (currentKeyCounts[c] === 0) {
+                $(this).addClass("unselected");
+            }
+
+            return false;
+        });
+
+        // star spirit trackers
+        $(`#chapter_${i}`).click(function(){
+            if ($(this).hasClass("unselected")) {
+                $(this).removeClass("unselected");
+            } else {
+                $(this).addClass("unselected");
+            }
+        });
+    }
+});
