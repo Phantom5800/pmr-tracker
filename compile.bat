4347e980156f60be0c71a3f3cc3b56667566076a
Phantom <phantom@phantom-games.com>
2022-08-03 22:31:56 -0700

    Simplify script compilation, TODO later: maybe break out scripts into more files

--- compile.bat
+++ compile.bat
@@ -1,5 +1,4 @@
 @echo off
 
-set SCRIPTS=scripts\main.js scripts\maps.js scripts\compact.js
 echo Compiling main.js ...
-java -jar closure-compiler-v20220719.jar --js %SCRIPTS% --js_output_file scripts\main.min.js
+java -jar closure-compiler-v20220719.jar --js scripts\*.js --js_output_file main.min.js
