69157909bbe7a756fda170f0ab905b1d19a45720
Phantom <phantom@phantom-games.com>
2022-08-08 23:10:26 -0700

    Add a vscode tasks.json for executing compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
 @echo off
 
-echo Compiling main.js ...
+echo Compiling main.min.js ...
 java -jar closure-compiler-v20220719.jar --js scripts\*.js --js_output_file main.min.js
+
+echo Compilation finished
