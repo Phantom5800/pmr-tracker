8ffc68733f9034700379eb6e521e75276f0819cb
Christian Legge <christian@christianlegge.dev>
2023-10-15 22:14:21 -0400

    fixed item order

--- src/data/items.ts
+++ src/data/items.ts
@@ -5,15 +5,6 @@ export const allItems = [
 		type: "chapterReward",
 		image: "/src/assets/images/icons/Eldstar_PM.png"
 	},
-
-	{
-		chapter: 1,
-		name: "Fortress Key",
-		type: "required",
-		image: "/src/assets/images/icons/FortressKey_PM.png",
-		multiple: 4
-	},
-
 	{
 		chapter: 2,
 		name: "Mamar",
@@ -21,59 +12,11 @@ export const allItems = [
 		image: "/src/assets/images/icons/Mamar_PM.png"
 	},
 	{
-		chapter: 2,
-		name: "Pulse Stone",
-		type: "required",
-		image: "/src/assets/images/icons/PulseStone.gif"
-	},
-	{
-		chapter: 2,
-		name: "Pyramid Stone",
-		type: "required",
-		image: "/src/assets/images/icons/PyramidStone.gif"
-	},
-	{
-		chapter: 2,
-		name: "Diamond Stone",
-		type: "required",
-		image: "/src/assets/images/icons/DiamondStone.png"
-	},
-	{
-		chapter: 2,
-		name: "Lunar Stone",
-		type: "required",
-		image: "/src/assets/images/icons/LunarStone.gif"
-	},
-
-	{
-		chapter: 2,
-		name: "Ruins Key",
-		type: "required",
-		image: "/src/assets/images/icons/Ruins_Key.png",
-		multiple: 4
-	},
-
-	{
 		chapter: 3,
 		name: "Skolar",
 		type: "chapterReward",
 		image: "/src/assets/images/icons/Skolar_PM.png"
 	},
-	{
-		chapter: 3,
-		name: "Boo's Portrait",
-		type: "required",
-		image: "/src/assets/images/icons/Boo'sPortrait_PM.png"
-	},
-
-	{
-		chapter: 3,
-		name: "Tubba Castle Key",
-		type: "required",
-		image: "/src/assets/images/icons/Tubba_Blubba_Castle_Key.png",
-		multiple: 3
-	},
-
 	{
 		chapter: 4,
 		name: "Muskular",
@@ -81,31 +24,11 @@ export const allItems = [
 		image: "/src/assets/images/icons/Muskular_PM.png"
 	},
 	{
-		chapter: 4,
-		name: "Toy Train",
-		type: "required",
-		image: "/src/assets/images/icons/ToyTrain_PM.png"
-	},
-	{
-		chapter: 4,
-		name: "Cake",
-		type: "required",
-		image: "/src/assets/images/icons/Cake.gif"
-	},
-
-	{
 		chapter: 5,
 		name: "Misstar",
 		type: "chapterReward",
 		image: "/src/assets/images/icons/Misstar_PM.png"
 	},
-	{
-		chapter: 5,
-		name: "Jade Raven",
-		type: "required",
-		image: "/src/assets/images/icons/JadeRaven_PM.png"
-	},
-
 	{
 		chapter: 6,
 		name: "Klevar",
@@ -113,97 +36,11 @@ export const allItems = [
 		image: "/src/assets/images/icons/Klevar_PM.png"
 	},
 	{
-		chapter: 6,
-		name: "Magical Seed 1",
-		type: "required",
-		image: "/src/assets/images/icons/MagicalSeed1.png"
-	},
-	{
-		chapter: 6,
-		name: "Magical Seed 2",
-		type: "required",
-		image: "/src/assets/images/icons/MagicalSeed2.png"
-	},
-	{
-		chapter: 6,
-		name: "Magical Seed 3",
-		type: "required",
-		image: "/src/assets/images/icons/MagicalSeed3.png"
-	},
-	{
-		chapter: 6,
-		name: "Magical Seed 4",
-		type: "required",
-		image: "/src/assets/images/icons/MagicalSeed4.png"
-	},
-	{
-		chapter: 6,
-		name: "Magical Bean",
-		type: "required",
-		image: "/src/assets/images/icons/MagicBean_PM.png"
-	},
-	{
-		chapter: 6,
-		name: "Fertile Soil",
-		type: "required",
-		image: "/src/assets/images/icons/Fertilesoil.png"
-	},
-	{
-		chapter: 6,
-		name: "Miracle Water",
-		type: "required",
-		image: "/src/assets/images/icons/MiracleWater_PM.png"
-	},
-
-	{
 		chapter: 7,
 		name: "Kalmar",
 		type: "chapterReward",
 		image: "/src/assets/images/icons/Kalmar_PM.png"
 	},
-
-	{
-		chapter: 7,
-		name: "Warehouse Key",
-		type: "required",
-		image: "/src/assets/images/icons/OddKey.gif",
-		label: "W"
-	},
-
-	{
-		chapter: 7,
-		name: "Bucket",
-		type: "required",
-		image: "/src/assets/images/icons/Bucket.png"
-	},
-	{
-		chapter: 7,
-		name: "Scarf",
-		type: "required",
-		image: "/src/assets/images/icons/Scarf.gif"
-	},
-	{
-		chapter: 7,
-		name: "Star Stone",
-		type: "required",
-		image: "/src/assets/images/icons/StarStone_PM.png"
-	},
-
-	{
-		chapter: 7,
-		name: "Red Key",
-		type: "required",
-		image: "/src/assets/images/icons/PM_Red_Key.png"
-	},
-
-	{
-		chapter: 7,
-		name: "Palace Key",
-		type: "required",
-		image: "/src/assets/images/icons/PM_Palace_Key.png",
-		label: "R"
-	},
-
 	{
 		chapter: 8,
 		name: "Star Rod",
@@ -216,33 +53,6 @@ export const allItems = [
 		type: "required",
 		image: "/src/assets/images/icons/Power_Star.png"
 	},
-
-	{
-		chapter: 8,
-		name: "Bowser's Castle Key",
-		type: "required",
-		image: "/src/assets/images/icons/PM_Bowser_Castle_Key.png"
-	},
-
-	{
-		chapter: 0,
-		name: "Boots",
-		type: "required",
-		image: "/src/assets/images/upgrades/PM_Boots.png"
-	},
-	{
-		chapter: 0,
-		name: "Hammer",
-		type: "required",
-		image: "/src/assets/images/upgrades/PM_Hammer.png"
-	},
-	{
-		chapter: 0,
-		name: "Ultra Stone",
-		type: "required",
-		image: "/src/assets/images/icons/UltraStone.gif"
-	},
-
 	{
 		chapter: -1,
 		name: "Goombario",
@@ -292,6 +102,24 @@ export const allItems = [
 		image: "/src/assets/images/partners/lakilester.png"
 	},
 	{
+		chapter: 0,
+		name: "Boots",
+		type: "required",
+		image: "/src/assets/images/upgrades/PM_Boots.png"
+	},
+	{
+		chapter: 0,
+		name: "Hammer",
+		type: "required",
+		image: "/src/assets/images/upgrades/PM_Hammer.png"
+	},
+	{
+		chapter: 0,
+		name: "Ultra Stone",
+		type: "required",
+		image: "/src/assets/images/icons/UltraStone.gif"
+	},
+	{
 		name: "Dolly",
 		type: "miscItem",
 		image: "/src/assets/images/icons/PeachDoll_PM.png"
@@ -302,12 +130,49 @@ export const allItems = [
 		image: "/src/assets/images/icons/Kooper'sShell_PM.png"
 	},
 	{
+		chapter: 1,
+		name: "Fortress Key",
+		type: "required",
+		image: "/src/assets/images/icons/FortressKey_PM.png",
+		multiple: 4
+	},
+	{
 		name: "Letters",
 		type: "miscItem",
 		image: "/src/assets/images/icons/PM_Letter_Sprite.png",
 		multiple: 3
 	},
-
+	{
+		chapter: 2,
+		name: "Pulse Stone",
+		type: "required",
+		image: "/src/assets/images/icons/PulseStone.gif"
+	},
+	{
+		chapter: 2,
+		name: "Pyramid Stone",
+		type: "required",
+		image: "/src/assets/images/icons/PyramidStone.gif"
+	},
+	{
+		chapter: 2,
+		name: "Diamond Stone",
+		type: "required",
+		image: "/src/assets/images/icons/DiamondStone.png"
+	},
+	{
+		chapter: 2,
+		name: "Lunar Stone",
+		type: "required",
+		image: "/src/assets/images/icons/LunarStone.gif"
+	},
+	{
+		chapter: 2,
+		name: "Ruins Key",
+		type: "required",
+		image: "/src/assets/images/icons/Ruins_Key.png",
+		multiple: 4
+	},
 	{
 		name: "Artifact",
 		type: "miscItem",
@@ -329,6 +194,31 @@ export const allItems = [
 		image: "/src/assets/images/icons/WeightPM.gif"
 	},
 	{
+		chapter: 3,
+		name: "Boo's Portrait",
+		type: "required",
+		image: "/src/assets/images/icons/Boo'sPortrait_PM.png"
+	},
+	{
+		chapter: 3,
+		name: "Tubba Castle Key",
+		type: "required",
+		image: "/src/assets/images/icons/Tubba_Blubba_Castle_Key.png",
+		multiple: 3
+	},
+	{
+		name: "Storeroom Key",
+		type: "miscKey",
+		image: "/src/assets/images/icons/OddKey.gif",
+		label: "S"
+	},
+	{
+		chapter: 4,
+		name: "Toy Train",
+		type: "required",
+		image: "/src/assets/images/icons/ToyTrain_PM.png"
+	},
+	{
 		name: "Calculator",
 		type: "miscItem",
 		image: "/src/assets/images/icons/Calculator_PM.png"
@@ -342,6 +232,12 @@ export const allItems = [
 		name: "Mailbag",
 		type: "miscItem",
 		image: "/src/assets/images/icons/Mailbag_PM.png"
+	},
+	{
+		chapter: 4,
+		name: "Cake",
+		type: "required",
+		image: "/src/assets/images/icons/Cake.gif"
 	},
 	{
 		name: "Cookbook",
@@ -359,9 +255,70 @@ export const allItems = [
 		image: "/src/assets/images/icons/MysteryNote.png"
 	},
 	{
+		chapter: 5,
+		name: "Jade Raven",
+		type: "required",
+		image: "/src/assets/images/icons/JadeRaven_PM.png"
+	},
+	{
 		name: "Volcano Vase",
 		type: "miscItem",
 		image: "/src/assets/images/icons/VolcanoVase.gif"
+	},
+	{
+		chapter: 6,
+		name: "Magical Seed 1",
+		type: "required",
+		image: "/src/assets/images/icons/MagicalSeed1.png"
+	},
+	{
+		chapter: 6,
+		name: "Magical Seed 2",
+		type: "required",
+		image: "/src/assets/images/icons/MagicalSeed2.png"
+	},
+	{
+		chapter: 6,
+		name: "Magical Seed 3",
+		type: "required",
+		image: "/src/assets/images/icons/MagicalSeed3.png"
+	},
+	{
+		chapter: 6,
+		name: "Magical Seed 4",
+		type: "required",
+		image: "/src/assets/images/icons/MagicalSeed4.png"
+	},
+	{
+		chapter: 6,
+		name: "Magical Bean",
+		type: "required",
+		image: "/src/assets/images/icons/MagicBean_PM.png"
+	},
+	{
+		name: "Red Berry",
+		type: "miscKey",
+		image: "/src/assets/images/icons/PaperMario_Items_RedBerry.png",
+		label: "R"
+	},
+	{
+		name: "Yellow Berry",
+		type: "miscKey",
+		image: "/src/assets/images/icons/PaperMario_Items_YellowBerry.png",
+		label: "Y"
+	},
+	{
+		name: "Blue Berry",
+		type: "miscKey",
+		image: "/src/assets/images/icons/PaperMario_Items_BlueBerry.png",
+		label: "B",
+		multiple: 2
+	},
+	{
+		chapter: 6,
+		name: "Fertile Soil",
+		type: "required",
+		image: "/src/assets/images/icons/Fertilesoil.png"
 	},
 	{
 		name: "Crystal Berry",
@@ -372,6 +329,82 @@ export const allItems = [
 		name: "Water Stone",
 		type: "miscItem",
 		image: "/src/assets/images/icons/WaterStone_PM.png"
+	},
+	{
+		chapter: 6,
+		name: "Miracle Water",
+		type: "required",
+		image: "/src/assets/images/icons/MiracleWater_PM.png"
+	},
+	{
+		name: "Bubble Berry",
+		type: "miscKey",
+		image: "/src/assets/images/icons/PaperMario_Items_BubbleBerry.png"
+	},
+	{
+		name: "Odd Key",
+		type: "miscKey",
+		image: "/src/assets/images/icons/OddKey.gif",
+		label: "O"
+	},
+	{
+		chapter: 7,
+		name: "Warehouse Key",
+		type: "required",
+		image: "/src/assets/images/icons/OddKey.gif",
+		label: "W"
+	},
+	{
+		chapter: 7,
+		name: "Scarf",
+		type: "required",
+		image: "/src/assets/images/icons/Scarf.gif"
+	},
+	{
+		chapter: 7,
+		name: "Bucket",
+		type: "required",
+		image: "/src/assets/images/icons/Bucket.png"
+	},
+	{
+		chapter: 7,
+		name: "Star Stone",
+		type: "required",
+		image: "/src/assets/images/icons/StarStone_PM.png"
+	},
+	{
+		name: "Blue Key",
+		type: "miscKey",
+		image: "/src/assets/images/icons/PM_BlueKey.png",
+		label: "B"
+	},
+	{
+		chapter: 7,
+		name: "Red Key",
+		type: "required",
+		image: "/src/assets/images/icons/PM_Red_Key.png",
+		label: "R"
+	},
+
+	{
+		chapter: 7,
+		name: "Palace Key",
+		type: "required",
+		image: "/src/assets/images/icons/PM_Palace_Key.png"
+	},
+	{
+		chapter: 8,
+		name: "Bowser's Castle Key",
+		type: "required",
+		image: "/src/assets/images/icons/PM_Bowser_Castle_Key.png",
+		multiple: 5
+	},
+	{
+		name: "Prison Key",
+		type: "miscKey",
+		image: "/src/assets/images/icons/OddKey.gif",
+		label: "P",
+		multiple: 2
 	},
 	{
 		name: "Lyrics",
@@ -409,55 +442,6 @@ export const allItems = [
 		image: "/src/assets/images/icons/Anti_Guy_Sprite_-_Paper_Mario.png"
 	},
 	{
-		name: "Red Berry",
-		type: "miscKey",
-		image: "/src/assets/images/icons/PaperMario_Items_RedBerry.png",
-		label: "R"
-	},
-	{
-		name: "Yellow Berry",
-		type: "miscKey",
-		image: "/src/assets/images/icons/PaperMario_Items_YellowBerry.png",
-		label: "Y"
-	},
-	{
-		name: "Blue Berry",
-		type: "miscKey",
-		image: "/src/assets/images/icons/PaperMario_Items_BlueBerry.png",
-		label: "B",
-		multiple: 2
-	},
-	{
-		name: "Bubble Berry",
-		type: "miscKey",
-		image: "/src/assets/images/icons/PaperMario_Items_BubbleBerry.png"
-	},
-	{
-		name: "Storeroom Key",
-		type: "miscKey",
-		image: "/src/assets/images/icons/OddKey.gif",
-		label: "S"
-	},
-	{
-		name: "Odd Key",
-		type: "miscKey",
-		image: "/src/assets/images/icons/OddKey.gif",
-		label: "O"
-	},
-	{
-		name: "Blue Key",
-		type: "miscKey",
-		image: "/src/assets/images/icons/PM_BlueKey.png",
-		label: "B"
-	},
-	{
-		name: "Prison Key",
-		type: "miscKey",
-		image: "/src/assets/images/icons/OddKey.gif",
-		label: "P",
-		multiple: 2
-	},
-	{
 		name: "Merlon",
 		type: "letter",
 		image: "/src/assets/images/Letters/PM_Merlon.png"
@@ -484,7 +468,6 @@ export const allItems = [
 	},
 	{
 		name: "Fice T.",
-
 		type: "letter",
 		image: "/src/assets/images/Letters/PM_Fice_T_Sprite.png"
 	},
