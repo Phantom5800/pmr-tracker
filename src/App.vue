8e865b1a960d16dfc84816123d046df85e742d9f
Christian Legge <christian@christianlegge.dev>
2023-10-24 01:49:33 -0400

    nicer looking buttons

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, computed, reactive, onMounted, onBeforeUnmount } from "vue";
 import EnabledSettings from "./components/EnabledSettings.vue";
 import InfoBlocks from "./components/InfoBlocks.vue";
+import Button from "./components/Button.vue";
 import { configKeys, settingsKeys } from "./stores/config";
 import MapTracker from "./components/MapTracker.vue";
 import ItemTracker from "./components/ItemTracker.vue";
@@ -351,35 +352,87 @@ const dragEndTimeout = (panelKey: keyof typeof panels) =>
 		{{ options.paperMarioFont ? "1.1rem" : "1rem" }} }
 	</component>
 	<header>
-		<div style="display: flex">
-			<div
-				id="options-menu-toggle"
-				:class="{ 'options-open': settingsOpen }"
-				class="hamburger"
+		<div style="display: flex; gap: 8px">
+			<Button
+				name="Tracker Config"
+				@click="
+					if (!configOpen) {
+						configOpen = true;
+					}
+					settingsOpen = false;
+				"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+					/>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+					/></svg
+			></Button>
+			<Button
+				name="Seed Settings"
 				@click="
 					settingsOpen = !settingsOpen;
 					configOpen = false;
 				"
 			>
-				<div class="hamburger-1"></div>
-				<div class="hamburger-2"></div>
-				<div class="hamburger-3"></div>
-			</div>
-			<button @click="moving = !moving">Edit Layout</button>
-			<button @click="resetLayout">Reset Layout</button>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+					/>
+				</svg>
+			</Button>
+			<Button name="Edit Layout" @click="moving = !moving">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z"
+					/>
+				</svg>
+			</Button>
+			<Button name="Reset Layout" @click="resetLayout"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9 9l6-6m0 0l6 6m-6-6v12a6 6 0 01-12 0v-3"
+					/>
+				</svg>
+			</Button>
 			<img id="logo-img" src="./assets/images/Logo.png" />
-		</div>
-		<div
-			id="settings-menu-toggle"
-			:class="{ 'options-open': configOpen }"
-			@click="
-				if (!configOpen) {
-					configOpen = true;
-				}
-				settingsOpen = false;
-			"
-		>
-			&#x2699;
 		</div>
 		<div
 			class="add-panels"
@@ -387,7 +440,22 @@ const dragEndTimeout = (panelKey: keyof typeof panels) =>
 				translate: moving ? undefined : '0 -10rem',
 			}"
 		>
-			<button @click="moving = false">Stop Editing</button>
+			<Button name="Lock Layout" @click="moving = false"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+					/>
+				</svg>
+			</Button>
 			<div
 				v-for="[key, panel] in Object.entries(panels).filter(
 					([k, v]) => !currentPanels.includes(k)
@@ -627,53 +695,6 @@ footer {
 	height: 40px;
 	cursor: unset;
 	padding-left: 7.5em;
-}
-
-#settings-menu-toggle {
-	cursor: pointer;
-	padding-right: 1em;
-	color: black;
-	font-size: 35px;
-	font-weight: bold;
-	-webkit-text-stroke-width: 0.125px;
-	-webkit-text-stroke-color: gainsboro;
-}
-
-#settings-menu-toggle.options-open {
-	color: gainsboro;
-	-webkit-text-stroke-color: black;
-}
-
-.hamburger {
-	cursor: pointer;
-	width: 40px;
-}
-
-.hamburger-1,
-.hamburger-2,
-.hamburger-3 {
-	width: 35px;
-	height: 3px;
-	background-color: black;
-	margin: 6px 0;
-	transition: 0.4s;
-	border-color: gainsboro;
-	border-width: 1px;
-	border-style: solid;
-}
-
-.options-open .hamburger-1 {
-	-webkit-transform: rotate(-45deg) translate(-9px, 6px);
-	transform: rotate(-45deg) translate(-9px, 6px);
-}
-
-.options-open .hamburger-2 {
-	opacity: 0;
-}
-
-.options-open .hamburger-3 {
-	-webkit-transform: rotate(45deg) translate(-8px, -8px);
-	transform: rotate(45deg) translate(-8px, -8px);
 }
 
 div.flex-col {
