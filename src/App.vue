43fb1c1447d6ee2bd2b17ea8907aca2fc8bddb27
Christian Legge <christian@christianlegge.dev>
2023-10-25 19:18:14 -0400

    added how to block

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ import type { Breakpoint, Layout } from "grid-layout-plus";
 import { throttle } from "lodash";
 import OverlayModal from "./components/OverlayModal.vue";
 import { usePlaythrough } from "./stores/playthrough";
+import InfoBlocks from "./components/InfoBlocks.vue";
 
 const breakpoint = ref<Breakpoint>("lg");
 const loadButton = ref<HTMLInputElement | null>(null);
@@ -153,7 +154,7 @@ function syncMousePosition(event: MouseEvent) {
 const layout = computed(() => layouts[breakpoint.value]);
 const currentPanels = computed(() => layout.value.map((el) => el.i));
 
-const openModal = ref<"settings" | "config" | "import" | null>(null);
+const openModal = ref<"settings" | "config" | "import" | "info" | null>(null);
 const moving = ref(false);
 
 const optionsStore = useOptions();
@@ -344,6 +345,14 @@ function doWithPrompt(prompt: string, fn: () => void): void {
 		fn();
 	}
 }
+
+function saveVisit() {
+	localStorage.setItem("visited", "true");
+}
+
+if (!localStorage.getItem("visited")) {
+	openModal.value = "info";
+}
 </script>
 
 <template>
@@ -352,6 +361,19 @@ function doWithPrompt(prompt: string, fn: () => void): void {
 		{{ options.paperMarioFont ? "Paper Mario" : "Open Sans" }};}
 	</component>
 
+	<OverlayModal
+		v-if="openModal === 'info'"
+		@close="
+			openModal = null;
+			saveVisit();
+		"
+	>
+		<InfoBlocks
+			@close="
+				openModal = null;
+				saveVisit();
+			"
+	/></OverlayModal>
 	<OverlayModal v-if="openModal === 'settings'" @close="openModal = null">
 		<p class="settings-warning">
 			Note: you probably don't need to set these manually! Use the Import Seed
@@ -447,6 +469,22 @@ function doWithPrompt(prompt: string, fn: () => void): void {
 		</div>
 		<img id="logo-img" src="./assets/images/Logo.png" />
 		<div class="buttons">
+			<SvgButton name="How to Use" @click="openModal = 'info'"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+					/>
+				</svg>
+			</SvgButton>
 			<SvgButton
 				name="Save Tracker Data"
 				@click="playthroughStore.savePlaythrough()"
--- src/components/InfoBlocks.vue
+++ src/components/InfoBlocks.vue
@@ -1,39 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineEmits } from "vue";
+
+defineEmits(["close"]);
+
+function getImageUrl(image: string) {
+	return new URL(`../assets/images/${image}`, import.meta.url).href;
+}
+</script>
 
 <template>
 	<div class="info-block">
-		<div>Tracker Usage:</div>
+		<img id="logo-img" :src="getImageUrl('Logo.png')" />
+		<p>by Phantom5800 and scatter</p>
 		<ul>
 			<li>
-				Make sure to mark the Boots and Hammer if you start with them, as the
-				tracker assumes you don't. Set your starting location also!
+				This is just a tracker. To generate a seed and play yourself, head over
+				to
+				<a href="https://pm64randomizer.com/" target="_blank"
+					>https://pm64randomizer.com/</a
+				>
+			</li>
+			<li>
+				Click the Import Seed button in the top bar and paste in the ID from the
+				pm64randomizer.com URL to import your seed settings. This will filter
+				certain items and map checks based on the seed logic.
 			</li>
 			<li>
 				Left clicking on any object toggles whether or not they are collected.
 			</li>
 			<li>
-				Left clicking on a key or upgrade (boots/hammer) increments the counter,
-				right clicking will decrement the counter.
+				Left clicking on the Boots, Hammer, or an item which you can get
+				multiples of increments the counter. Right clicking will decrement the
+				counter.
 			</li>
 			<li>
 				Star Spirits will highlight when all the requirements to acquire them
 				glitchlessly are met, as will the Star Rod.
 			</li>
 			<li>
-				Right clicking on a Star Spirit will increment a number from 1-7, that
-				can be useful for tracking chapter difficulty with the "shuffle
-				chapters" option in seeds.
+				Right clicking on a Star Spirit will allow you to set a chapter scaling
+				number from 1-7 and set an icon to track which dungeon is where in
+				Dungeon Shuffle.
+			</li>
+			<li>
+				Right clicking on a partner will allow you to track whether you've
+				upgraded them, and to which rank.
 			</li>
 			<li>
 				Right clicking on various "optional" items will add a checkmark to them,
-				this can be used to signify that they have been turned in. This will
+				which you can use to signify that they have been turned in. This will
 				mark the respective checks in the map tracker as well.
 			</li>
 			<li>
-				This site uses localstorage to remember your settings and progress! Some
-				browsers or plugins may disable this when you disable cookies as well.
-				This won't break anything, but you will have to manually re-enter your
-				settings each time and will lose progress if you refresh the page.
+				Click "Edit Layout" in the top bar to drag, resize, add and remove the
+				tracker panels. Your layout is saved automatically.
 			</li>
 			<li>
 				For suggestions and feature requests, please make issues on
@@ -42,26 +62,43 @@
 				>! If you don't have a Github, you can also reach out on any of the
 				social media in the footer.
 			</li>
-			<li>
-				This is just a tracker, to generate a seed and play yourself, head over
-				to
-				<a href="https://pm64randomizer.com/" target="_blank"
-					>https://pm64randomizer.com/</a
-				>
-			</li>
+			<li>To see this message again, click "How to Use" in the top bar.</li>
+			<button @click="$emit('close')">Get started</button>
 		</ul>
 	</div>
 </template>
 
 <style scoped>
 div.info-block {
-	width: 34em;
 	margin: 10px 10px 10px 10px;
 	color: white;
+	font-size: 1.5rem;
+	max-width: 80ch;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
+
+#logo-img {
+	width: 50%;
+}
+
+p {
+	font-size: 1.2rem;
+	margin-top: -1rem;
+}
+
 ul {
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
+}
+
+a {
+	color: white;
+}
+
+button {
+	font-size: 1.8rem;
 }
 </style>
