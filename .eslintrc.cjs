557886e6937f242447a2b6b57c5593d8387f3ed7
Christian Legge <christian@christianlegge.dev>
2023-10-27 12:03:32 -0400

    eslint ignore unused underscore args, dont fix automatically, error on any warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,12 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: "latest",
 	},
+	rules: {
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{
+				argsIgnorePattern: "^_+$",
+			},
+		],
+	},
 };
