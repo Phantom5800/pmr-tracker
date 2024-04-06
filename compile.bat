e62a62e82fad3f9b0e57c4f28e6f457efd4dd350
Phantom <phantom@phantom-games.com>
2024-04-06 02:53:04 -0700

    Update closure

--- compile.bat
+++ compile.bat
@@ -3,6 +3,6 @@
 set OPT=--compilation_level SIMPLE --env BROWSER
 
 echo Compiling main.min.js ...
-java -jar closure-compiler-v20221102.jar %OPT% --js scripts\*.js --externs scripts\externs\*.js --js_output_file main.min.js
+java -jar closure-compiler-v20240317.jar %OPT% --js scripts\*.js --externs scripts\externs\*.js --js_output_file main.min.js
 
 echo Compilation finished
